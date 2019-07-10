1ffc551b9649855918040dd1899c68ea0710a745
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-10 16:52:39 -0600

    Add passing tests to actions for save projects and palettes

--- src/actions/action.test.js
+++ src/actions/action.test.js
@@ -10,6 +10,37 @@ describe('actions', () => {
         };
         const result = actions.hasError(message)
         expect(result).toEqual(expected)
+    });
 
-    })
+    it('should return a type of SAVE_PROJECTS with all projects', () => {
+        const projects = mockProject;
+        const expected = {
+            type:'SAVE_PROJECTS',
+            projects
+        }
+        const result = actions.saveProjects(projects);
+        expect(result).toEqual(expected);
+    });
+
+    it('should return a type of SAVE_PALETTE with all projects', () => {
+        const palette = mockPalettes;
+        const expected = {
+            type:'SAVE_PALETTE',
+            palette
+        }
+        const result = actions.savePalette(palette);
+        expect(result).toEqual(expected);
+    });
+
+
+    it('should return a type of IS_LOADING with a boolean', () => {
+        const isLoading = true;
+        const expected = {
+            type:'IS_LOADING',
+            isLoading
+        }
+        const result = actions.isLoading(isLoading)
+        expect(result).toEqual(expected);
+    });
+
 })
