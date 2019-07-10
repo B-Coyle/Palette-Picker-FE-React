0229f64d6ccb2497b4ca6fb994047b3644165be7
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-10 15:03:22 -0600

    Start deplyment to Heroku

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { isLoadingReducer } from "./isLoadingReducer";
 import { errorReducer } from "./errorReducer";
 import { projectsReducer } from "./projectsReducer";
 import { palettesReducer } from "./palettesReducer";
+require('dotenv').config();
 
 export const rootReducer = combineReducers({
     isLoading: isLoadingReducer,
