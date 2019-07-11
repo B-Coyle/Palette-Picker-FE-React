8b892789a6cced847aeea08a3bd7ef98aa21ba6a
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-10 18:58:59 -0600

    Add tests to project Reducer

--- src/reducers/projectsReducer.test.js
+++ src/reducers/projectsReducer.test.js
@@ -1,4 +1,20 @@
-import { projectsReducer } from "./palettesReducer";
+import { projectsReducer } from "./projectsReducer";
 import * as actions from '../actions/index';
-import { mockPalettes } from '../thunks/mockData';
+import { mockProjects } from '../thunks/mockData';
+
+describe('projectsReducer', () => {
+
+    it('should return the initial state', () => {
+        const expected = []
+        const result = projectsReducer(undefined, {})
+        expect(result).toEqual(expected)
+    });
+
+    it('should return all of the projects', () => {
+        const initialState = []
+        const expected = mockProjects;
+        const result = projectsReducer(initialState, actions.addProject(expected))[0]
+        expect(result).toEqual(expected)
+    });
+})
 
