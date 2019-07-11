0e29cb8076f8c0518bf57e7a9dcda81f8912f029
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-10 19:15:14 -0600

    Add correct passing test in palettes Reducer

--- src/reducers/palettesReducer.test.js
+++ src/reducers/palettesReducer.test.js
@@ -3,6 +3,9 @@ import * as actions from '../actions/index';
 import { mockPalettes } from '../thunks/mockData';
 
 describe('paletteReducer' , () => {
+
+    let mockState = [];
+
     it('should return the initial state', () => {
         const expected = []
         const result = palettesReducer(undefined, {})
@@ -16,12 +19,18 @@ describe('paletteReducer' , () => {
         expect(result).toEqual(expected)
     });
 
+    it('should save a new project', () => {
+        const action = actions.savePalette(mockPalettes);
+        const result = palettesReducer(mockState, action)
+        expect(result).toEqual(mockPalettes)
+    });
+
     it('should add a new palette', () => {
         const initialState = [{ color1: 'white'}]
         const addedPalette = { color1: 'pink'}
         const expected = [{ color1: 'white'}, { color1: 'pink'}]
 
-        const result = palettesReducer(initialState, actions.savePalette(addedPalette))
+        const result = palettesReducer(initialState, actions.addPalette(addedPalette))
         expect(result).toEqual(expected)
     });
 
