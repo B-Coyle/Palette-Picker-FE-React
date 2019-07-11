92a80c2530f88200188cdc100b5b33d5728bbd9d
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-10 20:48:42 -0600

    Add passing test to fetch palette thunk

--- src/thunks/fetchPalettes.test.js
+++ src/thunks/fetchPalettes.test.js
@@ -1,5 +1,5 @@
 import { fetchPalettes } from "./fetchPalettes";
-import {savePalette, isLoading} from "../actions/index";
+import {savePalette, isLoading, hasError} from "../actions/index";
 import { mockPalettes } from './mockData';
 
 
@@ -16,6 +16,34 @@ describe("fetchPalettes Thunk", () => {
     const thunk = fetchPalettes(mockUrl, savePalette, "GET");
     thunk(mockDispatch);
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
+  });
+
+  it("should dispatch hasError with a message if the response is not ok", async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+        statusText: "Error has occurred"
+      })
+    );
+    const thunk = fetchPalettes(mockUrl, savePalette, "GET");
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(hasError("Error has occurred"));
+  });
+
+  it("should dispatch savePalette", async () => {
+    let mockPalettes = { palette_name: "Cool Spring Water" };
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockPalettes),
+        ok: true
+      })
+    );
+
+    const thunk = fetchPalettes(mockUrl, savePalette, "GET");
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(savePalette(mockPalettes));
   });
 
 
