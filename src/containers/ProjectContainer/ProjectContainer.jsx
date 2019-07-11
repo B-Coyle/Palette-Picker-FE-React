9e315d14493cf6ddca14fbdd8747406a44392931
jacobogart <jacobogart@gmail.com>
2019-07-11 08:55:41 -0600

    Update styling

--- src/containers/ProjectContainer/ProjectContainer.jsx
+++ src/containers/ProjectContainer/ProjectContainer.jsx
@@ -3,12 +3,15 @@ import { connect } from "react-redux";
 import Project from "../Project/Project";
 
 const ProjectContainer = props => {
-  return (
-    <section class="saved-projects-section">
-      {props.projects.length &&
+  const projectComponents = props.projects.length ?
         props.projects.reverse().map(project => (
           <Project key={project.id} project={project} />
-        ))}
+        ))
+        : <p>Please create a project.</p>
+
+  return (
+    <section class="saved-projects-section">
+      {projectComponents}
     </section>
   );
 };
